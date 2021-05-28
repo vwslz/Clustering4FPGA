16da20bdf28a24f15414fc77589fd7304e98f774
vwslz <vwslzk@gmail.com>
2021-05-28 13:36:19 -0400

    O(|E|)
    Although |E| is bounded by |N|^2, since the graph is sparse, it could be much smaller than |N|^2.

--- clustering.cpp
+++ clustering.cpp
@@ -23,9 +23,25 @@ void printIntArray(int* input) {
     }
 }
 
-void display(struct Node* input, int* conn) {
+void printNodeArray(struct Node* input) {
     for(int i = 0; i < MAX;i++) {
         printNode(input[i]);
+    }
+}
+
+void printNodeMatrix(struct Node** input) {
+    for(int i = 0; i < MAX; i++) {
+        printf("----");
+        printNodeArray(input[i]);
+    }
+}
+
+void printIntMatrix(int input[MAX][MAX]) {
+    for(int i = 0; i < MAX; i++) {
+        printf("\n");
+        for(int j = 0; j < MAX; j++) {
+            printf("%d ", input[i][j]);
+        }
     }
 }
 
@@ -37,82 +53,111 @@ void copyNode(struct Node* nodeTo, struct Node nodeFrom) {
 }
 
 void swapNode(struct Node* nodes, int i, int j) {
-   copyNode(&temp, nodes[i]);
-   copyNode(&(nodes[i]), nodes[j]);
-   copyNode(&(nodes[j]), temp);
+  copyNode(&temp, nodes[i]);
+  copyNode(&(nodes[i]), nodes[j]);
+  copyNode(&(nodes[j]), temp);
 }
 
-void getCluster(struct Node* nodes, int* status) {
-    // printf("\n----Get Cluster----");
+void getAdjancyMatrix(int adjMatrix[MAX][MAX], struct Node* input) {
+    for (int i = 0; i < MAX; i++) {
+        struct Node node_iter_i;
+        copyNode(&node_iter_i, input[i]);
+        struct Node node_iter_j;
+        for (int j = 0; j < MAX; j++) {
+            if (i == j)
+                continue;
+            copyNode(&node_iter_j, input[j]);
+            if (node_iter_i.layer_id == node_iter_j.layer_id
+                && abs(node_iter_i.phi_index - node_iter_j.phi_index) <= 2
+                && abs(node_iter_i.z_index - node_iter_j.z_index) <= 2) {
+                adjMatrix[i][j] = 1;
+            }
+        }
+    }
+}
+
+void getAdjancyList(int adjList[MAX][MAX], int adjMatrix[MAX][MAX], struct Node* input) {
+    int adjListIndex[MAX] = {0,0,0,0,0,0,0,0};
+    for (int i = 0; i < MAX; i++) {
+        for (int j = i; j < MAX; j++) {
+            if (adjMatrix[i][j] == 1)
+                adjList[i][adjListIndex[i]++] = j;
+        }
+    }
+}
+
+void getCluster(struct Node output[MAX], struct Node input[MAX], int adjList[MAX][MAX], int marked[MAX]) {
     
-    printf("\nStatus:");
-    printIntArray(status);
     int ptr_cluster = 0;
     int ptr_next = 0;
-    struct Node node_iter;
-    
     
     while (ptr_cluster < MAX) {
-        printf("\n---------------------");
-        copyNode(&node_iter, nodes[ptr_cluster]);
-        printf("\nFind neighbours for %d at %d", node_iter.id, ptr_cluster);
-        if (status[node_iter.id] == -1) {
-            printf("\nMark %d as begin of cluster", node_iter.id);
-            status[node_iter.id] = 1;
-            ptr_next++;
-        }
-        printNode(node_iter);
-        
-        for (int i = ptr_next; i < MAX ; i++) {
-            struct Node neighbour_iter;
-            copyNode(&neighbour_iter, nodes[i]);
-            // printf("\n Check neighbours at %d", i);
-            // printNode(neighbour_iter);
-            if (status[neighbour_iter.id] == -1
-                && node_iter.layer_id == neighbour_iter.layer_id
-                && abs(node_iter.phi_index - neighbour_iter.phi_index) <= 2
-                && abs(node_iter.z_index - neighbour_iter.z_index) <= 2) {
-                printf("\n--> %d add to %d", neighbour_iter.id, ptr_next);
-                
-                status[neighbour_iter.id] = 0;
-                swapNode(nodes, i, ptr_next++);
+        copyNode(&(output[ptr_next++]), input[ptr_cluster]);
+        marked[ptr_cluster] = 1;
+        for (int i = 0; i < MAX && adjList[ptr_cluster][i] > -1; i++) {
+            if (marked[adjList[ptr_cluster][i]] == 0) {
+                copyNode(&(output[ptr_next++]), input[adjList[ptr_cluster][i]]);
+                marked[adjList[ptr_cluster][i]] = 1;
             }
-            if (ptr_next == MAX)
-                return;
         }
-        
-        printf("\nStatus: ");
-        printIntArray(status);
-        ptr_cluster++;
-        printf("\nindexes: ");
-        printf("\n%d %d:", ptr_cluster, ptr_next);
+        while(marked[ptr_cluster] == 1)
+            ptr_cluster++;
     }
 
 }
 
 int main() {
-    struct Node input[] = {
-        {.id=0, .layer_id=0, .phi_index=1675, .z_index=3266},
-        {.id=1, .layer_id=0, .phi_index=1675, .z_index=3267},
-        {.id=2, .layer_id=0, .phi_index=1227, .z_index=4582},
-        {.id=3, .layer_id=0, .phi_index=4120, .z_index=3450},
-        {.id=4, .layer_id=0, .phi_index=1676, .z_index=3266},
-        {.id=5, .layer_id=0, .phi_index=1226, .z_index=4581},
-        {.id=6, .layer_id=0, .phi_index=1227, .z_index=4586},
-        {.id=7, .layer_id=0, .phi_index=1227, .z_index=4581},
+    struct Node input[MAX] = {
+        {.id=0, .layer_id=0, .phi_index=1676, .z_index=3266},
+        {.id=1, .layer_id=0, .phi_index=1227, .z_index=4582},
+        {.id=2, .layer_id=0, .phi_index=4120, .z_index=3450},
+        {.id=3, .layer_id=0, .phi_index=1226, .z_index=4581},
+        {.id=4, .layer_id=0, .phi_index=1227, .z_index=4586},
+        {.id=5, .layer_id=0, .phi_index=1675, .z_index=3266},
+        {.id=6, .layer_id=0, .phi_index=1227, .z_index=4581},
+        {.id=7, .layer_id=0, .phi_index=1675, .z_index=3267},
     };
     
     // -1, not used, 0, begin of a cluster, 1 used
     int status[MAX] = {-1, -1, -1, -1, -1, -1, -1, -1}; 
     
     printf("\nInput: ");
-    display(input, status);
-
-    getCluster(input, status);
-    printf("\n\nOutput: ");
-    display(input, status);
+    printNodeArray(input);
     
-    printf("\Status: ");
-    printIntArray(status);
+    int adjMatrix[MAX][MAX] = {
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0}
+    };
+    int adjList[MAX][MAX] = {
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+        {-1,-1,-1,-1,-1,-1,-1,-1},
+    };
+    
+    getAdjancyMatrix(adjMatrix, input);
+    printf("\nGet Adjancy Matrix: ");
+    printIntMatrix(adjMatrix);
+    
+    getAdjancyList(adjList, adjMatrix, input);
+    printf("\nGet Adjancy List: ");
+    printIntMatrix(adjList);
+
+    struct Node output[MAX];
+    int marked[MAX] = {0,0,0,0,0,0,0,0};
+    getCluster(output, input, adjList, marked);
+    printf("\n\nOutput: ");
+    printNodeArray(output);
+    
     return 0;
 }
